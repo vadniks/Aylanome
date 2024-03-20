28b7494941b8c13a6c3bb00016507404942106b5
vadniks <whatever@fake.some>
2024-03-20 11:53:57 +0300

    Structure, logic & UI

--- lib/presenter/MainPagePresenter.dart
+++ lib/presenter/MainPagePresenter.dart
@@ -9,7 +9,6 @@ class MainPagePresenter extends State<MainPage> {
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(
-      backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
       title: const Text('Title'),
     ),
     body: const Column(
