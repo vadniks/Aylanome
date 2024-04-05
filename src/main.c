0edfcc3c88b1f004ba9a0fb6ca48e6bde2c16931
vadniks <whatever@fake.some>
2024-04-05 14:34:00 +0300

    Structure & logic

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@ int main(void) {
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
@@ -36,12 +36,50 @@ int main(void) {
     if (!mainWindow) abort();
 
     mainContext = SDL_GL_CreateContext(mainWindow);
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) abort();
     SDL_GL_SetSwapInterval(1); // This makes our buffer swap syncronized with the monitor's vertical refresh
 
-    SDL_Delay(50);
+    SDL_Delay(1000);
 
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // Clear our buffer with a red background
-    glClear(GL_COLOR_BUFFER_BIT);
+    {
+        GLuint VertexArrayID;
+        glGenVertexArrays(1, &VertexArrayID);
+        glBindVertexArray(VertexArrayID);
+
+        const GLfloat g_vertex_buffer_data[] = {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f,  1.0f, 0.0f,
+        };
+
+        // This will identify our vertex buffer
+        GLuint vertexbuffer;
+// Generate 1 buffer, put the resulting identifier in vertexbuffer
+        glGenBuffers(1, &vertexbuffer);
+// The following commands will talk about our 'vertexbuffer' buffer
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+// Give our vertices to OpenGL.
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+        // 1st attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+            3,                  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void*)0            // array buffer offset
+        );
+// Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDisableVertexAttribArray(0);
+    }
+
+//    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+//    glClear(GL_COLOR_BUFFER_BIT);
     SDL_GL_SwapWindow(mainWindow); // Swap our back buffer to the front
     SDL_Delay(2000); // Wait 2 seconds
 
